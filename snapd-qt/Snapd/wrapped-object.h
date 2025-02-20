bb50657a4c48759fabed1fbb879b3a50a37fc1ed
Sergio Costas Rodriguez <sergio.costas@canonical.com>
2025-02-20 16:53:40 +0100

    Replace zero with NULL

--- snapd-qt/Snapd/wrapped-object.h
+++ snapd-qt/Snapd/wrapped-object.h
@@ -17,7 +17,7 @@ class Q_DECL_EXPORT QSnapdWrappedObject : public QObject {
 
 public:
   explicit QSnapdWrappedObject(void *object, void (*unref_func)(void *),
-                               QObject *parent = 0)
+                               QObject *parent = NULL)
       : QObject(parent), wrapped_object(object), unref_func(unref_func) {}
   ~QSnapdWrappedObject() { unref_func(wrapped_object); }
 
